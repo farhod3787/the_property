411d9ca4f2975668e544f0c2b8716fe14f299096
farhod3787 <farhod.marufjonov@mail.ru>
2019-09-14 18:37:01 +0500

    status

--- src/app/admin/admin-home/admin-home.component.ts
+++ src/app/admin/admin-home/admin-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/service/userService';
+import { AnnouncService } from 'src/app/shared/service/annouenceService';
 
 @Component({
   selector: 'app-admin-home',
@@ -8,7 +10,41 @@ import { Router } from '@angular/router';
 })
 export class AdminHomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  users = [];
+  announcs = []
+
+  q =0
+  a = 0;
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private announcService : AnnouncService
+    
+    ) { 
+        this.allUsers();
+        this.allAnnouncs();
+    }
+
+    allUsers() {
+        this.userService.getUsers().subscribe ( res =>{
+          this.users = res.json().length
+        })
+    }
+
+    allAnnouncs() {
+      this.announcService.getAnn().subscribe( res =>{
+        this.announcs = res.json()
+        for ( let i=0; i<=this.announcs.length; i++) {
+          if (this.announcs[i].status) {
+            this.q++
+          }          
+          else {
+            this.a++
+          }
+        }
+      }
+        )
+    }
 
   ngOnInit() {
   }
